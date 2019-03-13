34706c0dba3d38df5e540e66eef0488e675c0377
romanski <roman.reibnagel@gmail.com>
2019-03-13 19:41:56 +0100

    testing

---
+++ oebb/__init__.py
@@ -0,0 +1,3 @@
+"""Module to parse gearbest site"""
+
+from .oebb import OeBB
--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 
 setup(name='oebb',
       version='1.0.1.dev',
+      packages=['oebb'],
+      python_requires='>=3.6',
       description='Python API client for Austrian Federal Railways',
       url='https://github.com/arahorn28/oebb',
       license='MIT',
